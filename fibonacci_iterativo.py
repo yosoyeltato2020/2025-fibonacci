6d884455640174aed174e6ab82de7d4c817340ee
yosoyeltato2020 <yosoyeltato2020@hotmail.com>
2025-04-04 13:27:59 +0200

    fibonacci iterativo

---
+++ fibonacci_iterativo.py
@@ -0,0 +1,3 @@
+# calcula hasta el enesimo término de la serie de Fibonacci [iterativa, recursiva]
+# y lo imprime en la salida estandar
+# calcula hasta que el termino sea >= al numero dado de entrada [iterativa, recursiva]
