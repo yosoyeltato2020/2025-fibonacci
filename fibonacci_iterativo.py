a19824cb655ee1519c66b323aa78e471e5a273ab
yosoyeltato2020 <yosoyeltato2020@hotmail.com>
2025-04-04 13:36:30 +0200

    me equivoque al cargar el archivo ahora esto subiendo el archivo debido

--- fibonacci_iterativo.py
+++ fibonacci_iterativo.py
@@ -1,3 +1,16 @@
-# calcula hasta el enesimo término de la serie de Fibonacci [iterativa, recursiva]
-# y lo imprime en la salida estandar
-# calcula hasta que el termino sea >= al numero dado de entrada [iterativa, recursiva]
+def fibonacci_iterativo(n):
+    secuencia = []
+    a= 0
+    b= 1
+    for i in range(n):  
+        secuencia.append(a)
+        c = a + b
+        a = b
+        b = c
+        
+        
+    return secuencia
+
+# sin input y poner n = 8 para la enesima posicion.
+n = int(input("Introduce el número de posiciones para la secuencia hasta donde quieras llegar: "))
+print(f"Secuencia de Fibonacci (Iterativa): {fibonacci_iterativo(n)}")
